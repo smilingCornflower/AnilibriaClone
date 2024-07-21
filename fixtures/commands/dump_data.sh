7b06932022faa32fa3e8277f72ae415a03550ff6
Nauzybek Turganbayev <nauzybekturganbayev@MBA.local>
2024-07-22 02:06:05 +0500

    fixtures are added for test

---
+++ fixtures/commands/dump_data.sh
@@ -0,0 +1,8 @@
+python manage.py dumpdata anime.Voice > fixtures/voices.json
+python manage.py dumpdata anime.Genre > fixtures/genres.json
+python manage.py dumpdata anime.Timing > fixtures/timings.json
+python manage.py dumpdata anime.Subtitles > fixtures/subtitles.json
+python manage.py dumpdata anime.Episode > fixtures/episodes.json
+python manage.py dumpdata anime.Anime > fixtures/anime.json
+python manage.py dumpdata main.YouTubeVideo > fixtures/youtube.json
+python manage.py dumpdata users.User > fixtures/users.json
